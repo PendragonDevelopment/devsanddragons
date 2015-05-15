cb93caa5b5dcdae76cd4389856b1c69daa93be3a
Jordan Burke <me@jordankburke.com>
2015-05-15 19:20:47 -0400

    Added initial profile model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515182801) do
+ActiveRecord::Schema.define(version: 20150515230742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "twitter"
+    t.string   "facebook"
+    t.string   "github"
+    t.integer  "xp"
+    t.string   "current_title"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -51,4 +64,5 @@ ActiveRecord::Schema.define(version: 20150515182801) do
   add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "profiles", "users"
 end
---
+++ spec/factories/profiles.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :profile do
+    user nil
+twitter "MyString"
+facebook "MyString"
+github "MyString"
+xp 1
+current_title "MyString"
+  end
+
+end
