4236856a0977d51c9f1dc2e9edbe9c2125dcc8a7
Jordan Burke <me@jordankburke.com>
2015-05-16 18:24:25 -0400

    Redirect to Profiles#edit on sign up

--- config/routes.rb
+++ config/routes.rb
@@ -98,6 +98,6 @@ Rails.application.routes.draw do
   resources :quests
 
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   resources :users
 end
