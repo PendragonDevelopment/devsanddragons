e1daeb99ff590485d1145cbc706a44909090e6dd
Sean R Reid <sean@torchcodelab.com>
2015-05-16 16:33:18 -0400

    updated the sidebar, started incorporating the header and footer elements

--- patternlab/Gruntfile.js
+++ patternlab/Gruntfile.js
@@ -54,7 +54,7 @@ module.exports = function(grunt) {
     connect: {
       server: {
         options: {
-          port: 8080,
+          port: 8081,
           hostname: 'localhost',
           base: 'public/'
         }
